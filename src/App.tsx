9ef0f3628c766ecf908af0b7f917d0dd3cff64f6
Aderbal Ferreira <aderbalferreira09@gmail.com>
2022-07-02 13:50:26 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -46,11 +46,10 @@ export function App() {
           {posts.map(post => {
             return (
              <Post
-              key={post.id}
-              author={post.author}
-              content={post.content}
-              publishedAt={post.publishedAt}
-              /> 
+                key={post.id}
+                author={post.author}
+                publishedAt={post.publishedAt} content={[]}              
+            /> 
             ) 
           })}
         </main>
